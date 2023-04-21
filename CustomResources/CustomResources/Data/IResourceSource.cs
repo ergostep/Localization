b6473878764d9b095402dfaa7e0ac209ea755d4d
ergostep <52520230+ergostep@users.noreply.github.com>
2023-04-21 17:44:23 +0500

    Add Xml localization

---
+++ CustomResources/CustomResources/Data/IResourceSource.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+namespace CustomResources.Data;
+
+public interface IResourceSource
+{
+    public string GetString(string guid, CultureInfo culture);
+    public void AddString(string guid, string value, CultureInfo culture);
+    public void AddCulture(CultureInfo culture);
+
+}
--- CustomResources/CustomResources/Program.cs
+++ CustomResources/CustomResources/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Globalization;
+using CustomResources.Data;
+
+var ru = CultureInfo.GetCultureInfo("ru");
+var fr = CultureInfo.GetCultureInfo("fr");
+string guid = Guid.NewGuid().ToString();
+
+var xmlResource = new XmlResource();
+xmlResource.AddCulture(ru);
+xmlResource.AddCulture(fr);
+xmlResource.AddString(guid, "Утро", ru);
+xmlResource.AddString(guid, "Matin", fr);
+xmlResource.AddString(guid, "Morning");
+var locMan = new CustomLocalizationManager();
+locMan.RegisterResourceSource(xmlResource);
+Console.WriteLine($"For {CultureInfo.CurrentUICulture.ToString()} : {locMan.GetString(guid)}");
+Console.WriteLine($"For {ru} : {locMan.GetString(guid, ru)}");
+Console.WriteLine($"For {fr} : {locMan.GetString(guid, fr)}");
